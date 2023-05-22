1525b4d9bd0bf097a34bcce00682e8d2d9c6481e
irem.kutluca <irm.ktlc@gmail.com>
2023-05-22 23:51:53 +0300

    mimari sistem

---
+++ src/models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    role: String,
+    status: String,
+    created_at: Date,
+    updated_at: Date
+}); 
+
+module.exports = mongoose.model('User', userSchema);
